a98df3e7e6c917b12e4b70777f98fb6b75cde258
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2022-01-14 16:02:14 +0100

    Fix loading of .mocharc for client.
    See https://github.com/Meteor-Community-Packages/meteor-mocha-core/issues/6#issuecomment-1013208944
    
    Fixes #6.
    
    And possibly https://github.com/Meteor-Community-Packages/meteor-mocha/issues/110

--- client.js
+++ client.js
@@ -1,19 +1,13 @@
 // We need to import the "mocha.js" file specifically because that is the browser entry point.
 import 'mocha/mocha.js';
 
+import config from './loadConfig';
+
 // This defines "describe", "it", etc.
 let options = {
   ui: 'bdd',
+  ...config,
 };
-
-
-// Attempt to load config from .mocharc file
-try {
-  const config = JSON.parse(__meteor_runtime_config__['meteortesting:mocha-core_config']);
-  options = { ...options, ...config };
-} catch (e) {
-  console.error(e);
-}
 
 mocha.setup(options);
 
